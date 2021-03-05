c330dccee5b4a645c3ddd7c4913c940b15541121
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-03-05 13:38:13 +0200

    e11 sol

--- src/exercises/11/declarations/str-utils/index.d.ts
+++ src/exercises/11/declarations/str-utils/index.d.ts
@@ -1,4 +1,10 @@
 declare module 'str-utils' {
     // export const ...
     // export function ...
+    type StringMan = (value:string) => string;
+    export const stringReverse:StringMan;
+    export const stringToLower:StringMan;
+    export const stringToUpper:StringMan;
+    export const stringRandomize:StringMan;
+    export const stringInvertCase:StringMan;
 }
