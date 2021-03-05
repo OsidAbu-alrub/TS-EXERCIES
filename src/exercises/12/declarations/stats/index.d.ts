63e74195d810cde62ab4da0adb553c72e6182ea9
Osid Abu-Alrub <abualrub.osid@gmail.com>
2021-03-05 14:01:34 +0200

    e12 sol

--- src/exercises/12/declarations/stats/index.d.ts
+++ src/exercises/12/declarations/stats/index.d.ts
@@ -1,3 +1,12 @@
 declare module 'stats' {
-    export function getMaxIndex(input: unknown, comparator: unknown): unknown;
+    type NumberOrNull<T> = T | null;
+    type Func =  <T>(input: T[], comparator: (arg1?: T,arg2?:T) => number) => NumberOrNull<T>;
+
+    export const getMaxIndex:Func;
+    export const getMaxElement:Func;
+    export const getMinIndex:Func;
+    export const getMinElement:Func;
+    export const getMedianIndex:Func;
+    export const getMedianElement:Func;
+    export const getAverageValue:Func
 }
